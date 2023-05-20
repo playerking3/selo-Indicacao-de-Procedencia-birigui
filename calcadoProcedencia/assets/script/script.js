9a2b9902d16afd4b8cd874d761cf2f9364a706c5
playerking3 <murilomarquesdecastro@gmail.com>
2023-05-20 08:15:46 -0300

    fiz até a parte de beneficios

--- calcadoProcedencia/assets/script/script.js
+++ calcadoProcedencia/assets/script/script.js
@@ -4,3 +4,23 @@ function sobeHeader(){
         $("#conteudo").fadeIn("slow")
     });
 }
+
+$('.nav a[href^="#"]').on('click', function(e) {
+  e.preventDefault();
+  var id = $(this).attr('href'),
+  targetOffset = $(id).offset().top;
+    
+  $('html, body').animate({ 
+    scrollTop: targetOffset - 100
+  }, 500);
+});
+
+function colorido(img){
+    link = img.attributes.getNamedItem("src").value.replace("white","cor")
+    img.attributes.getNamedItem("src").value = link
+}
+
+function pretoBranco(img){
+    link = img.attributes.getNamedItem("src").value.replace("cor","white")
+    img.attributes.getNamedItem("src").value = link
+}
