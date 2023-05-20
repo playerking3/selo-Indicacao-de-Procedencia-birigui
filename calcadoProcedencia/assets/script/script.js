150077cd97ae7730b02aa794aa0223c2dcd458a8
playerking3 <murilomarquesdecastro@gmail.com>
2023-05-20 13:02:38 -0300

    fiz o seletor de section do header

--- calcadoProcedencia/assets/script/script.js
+++ calcadoProcedencia/assets/script/script.js
@@ -1,8 +1,31 @@
 function sobeHeader(){
     //Gambiarra do Igor, NÃO MEXER
     $("#some").slideUp("slow", function(){
-        $("#conteudo").fadeIn("slow")
+        $("#conteudo").fadeIn("slow", () =>{
+            for (var i = 0; i < a.length; i++) {
+                linkInfo.push(a[i].getBoundingClientRect())
+            }
+            sublinha.style.top = linkInfo[0].bottom - 3 + "px"
+            sublinha.style.width = linkInfo[0].width + "px"
+            sublinha.style.left = linkInfo[0].x + "px"
+            document.addEventListener("scroll",() => {
+                if(introducao.getBoundingClientRect().bottom > 0) {
+                    sublinha.style.top = linkInfo[0].bottom - 3 + "px"
+                    sublinha.style.width = linkInfo[0].width + "px"
+                    sublinha.style.left = linkInfo[0].x + "px"
+                }else if(que.getBoundingClientRect().bottom > 0){
+                    sublinha.style.top = linkInfo[1].bottom - 3 + "px"
+                    sublinha.style.width = linkInfo[1].width + "px"
+                    sublinha.style.left = linkInfo[1].x + "px"
+                }else if(beneficios.getBoundingClientRect().bottom > 0){
+                    sublinha.style.top = linkInfo[2].bottom - 3 + "px"
+                    sublinha.style.width = linkInfo[2].width + "px"
+                    sublinha.style.left = linkInfo[2].x + "px"
+                }
+            })
+        })
     });
+    
 }
 
 $('.nav a[href^="#"]').on('click', function(e) {
@@ -24,3 +47,11 @@ function pretoBranco(img){
     link = img.attributes.getNamedItem("src").value.replace("cor","white")
     img.attributes.getNamedItem("src").value = link
 }
+
+let que = document.getElementById("que")
+let introducao = document.getElementById("introducao")
+let beneficios = document.getElementById("beneficios")
+
+let linkInfo = []
+let sublinha = document.getElementById('sublinhado')
+let a = document.getElementsByClassName('linkes')
