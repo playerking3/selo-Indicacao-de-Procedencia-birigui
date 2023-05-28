2a62dc338b762675eb09b6fa4a4c20576d5ae7bc
playerking3 <murilomarquesdecastro@gmail.com>
2023-05-28 09:16:39 -0300

    responsivo completo
    provavelmente o projeto em si está  completo também

--- calcadoProcedencia/assets/script/script.js
+++ calcadoProcedencia/assets/script/script.js
@@ -1,3 +1,14 @@
+let que = document.getElementById("que")
+let introducao = document.getElementById("introducao")
+let beneficios = document.getElementById("beneficios")
+let selo = document.getElementById("selo")
+let componentes = document.getElementById("componentes")
+let uso = document.getElementById("uso")
+let linkInfo = []
+let sublinha = document.getElementById('sublinhado')
+let a = document.getElementsByClassName('linkes')
+let i = 0
+let headerMob = document.getElementsByClassName('mobP')
 function sobeHeader(){
     //Gambiarra do Igor, NÃO MEXER
     $("#some").slideUp("slow", function(){
@@ -5,34 +16,42 @@ function sobeHeader(){
             for (var i = 0; i < a.length; i++) {
                 linkInfo.push(a[i].getBoundingClientRect())
             }
+            console.log(headerMob)
             sublinha.style.top = linkInfo[0].bottom - 3 + "px"
             sublinha.style.width = linkInfo[0].width + "px"
             sublinha.style.left = linkInfo[0].x + "px"
+            scrollMobile(headerMob[0],headerMob)
             document.addEventListener("scroll",() => {
                 if(introducao.getBoundingClientRect().bottom > 0) {
                     sublinha.style.top = linkInfo[0].bottom - 3 + "px"
                     sublinha.style.width = linkInfo[0].width + "px"
                     sublinha.style.left = linkInfo[0].x + "px"
+                    scrollMobile(headerMob[0],headerMob)
                 }else if(que.getBoundingClientRect().bottom > 0){
                     sublinha.style.top = linkInfo[1].bottom - 3 + "px"
                     sublinha.style.width = linkInfo[1].width + "px"
                     sublinha.style.left = linkInfo[1].x + "px"
+                    scrollMobile(headerMob[1],headerMob)
                 }else if(beneficios.getBoundingClientRect().bottom > 0){
                     sublinha.style.top = linkInfo[2].bottom - 3 + "px"
                     sublinha.style.width = linkInfo[2].width + "px"
                     sublinha.style.left = linkInfo[2].x + "px"
+                    scrollMobile(headerMob[2],headerMob)
                 }else if(selo.getBoundingClientRect().bottom > 0){
                     sublinha.style.top = linkInfo[3].bottom - 3 + "px"
                     sublinha.style.width = linkInfo[3].width + "px"
                     sublinha.style.left = linkInfo[3].x + "px"
-                }else if(componentes.getBoundingClientRect().bottom > 0){
+                    scrollMobile(headerMob[3],headerMob)
+                }else if(componentes.getBoundingClientRect().bottom > 100){
                     sublinha.style.top = linkInfo[4].bottom - 3 + "px"
                     sublinha.style.width = linkInfo[4].width + "px"
                     sublinha.style.left = linkInfo[4].x + "px"
-                }else if(uso.getBoundingClientRect().top > 0){
+                    scrollMobile(headerMob[4],headerMob)
+                }else{
                     sublinha.style.top = linkInfo[5].bottom - 3 + "px"
                     sublinha.style.width = linkInfo[5].width + "px"
                     sublinha.style.left = linkInfo[5].x + "px"
+                    scrollMobile(headerMob[5],headerMob)
                 }
             })
             carrossel()
@@ -61,18 +80,6 @@ function pretoBranco(img){
     img.attributes.getNamedItem("src").value = link
 }
 
-let que = document.getElementById("que")
-let introducao = document.getElementById("introducao")
-let beneficios = document.getElementById("beneficios")
-let selo = document.getElementById("selo")
-let componentes = document.getElementById("componentes")
-let uso = document.getElementById("uso")
-
-let linkInfo = []
-let sublinha = document.getElementById('sublinhado')
-let a = document.getElementsByClassName('linkes')
-let i = 0
-
 function carrossel(){
     let reparticao = document.getElementsByClassName("reparticao")
     let explicacao = document.getElementsByClassName('explicacao')
@@ -93,4 +100,18 @@ function carrossel(){
         i++
     }
     setTimeout(carrossel, 5000)
+}
+
+function scrollMobile(p,tudo){
+    let resto = []
+    for (var i = 0; i < tudo.length; i++) {
+        if (tudo[i] === p) {
+            p.style.display = "block"
+        }else{
+            resto.push(i)
+        }
+    }
+    for (var j = 0; j < resto.length; j++) {
+        tudo[resto[j]].style.display = 'none'
+    }
 }
